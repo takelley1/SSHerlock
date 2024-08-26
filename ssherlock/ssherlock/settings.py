f085ff84466a92b6b6a4d145c07227247595c449
Austin Kelley <hxyz@protonmail.com>
2024-08-26 14:31:32 -0400

    ssherlock: add django-htmlmin dependency

--- ssherlock/ssherlock/settings.py
+++ ssherlock/ssherlock/settings.py
@@ -9,11 +9,13 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+#  HTML_MINIFY = True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -40,6 +42,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    #  "htmlmin.middleware.HtmlMinifyMiddleware",
+    #  "htmlmin.middleware.MarkRequestMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
