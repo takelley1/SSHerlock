9dd64451b81f43f17afcd5afaba5368be8285008
Austin Kelley <hxyz@protonmail.com>
2024-08-30 13:53:24 -0400

    ssherlock_runner: update code, add tests

---
+++ ssherlock_runner/ssherlock_runner.py
@@ -0,0 +1,458 @@
+import logging as log
+import os
+import time
+
+import fabric
+import openai
+import tiktoken
+
+# TODO: investigate if the LLM API model is necessary to change
+# Useful environment variables to include in the SSH commands.
+
+
+def log_function_call(func):
+    """Easily decorage functions with @log_function_call to log calls."""
+
+    def wrapper(*args, **kwargs):
+        log.debug("Calling %s with args=%s, kwargs=%s", func.__name__, args, kwargs)
+        result = func(*args, **kwargs)
+        log.debug("%s returned %s", func.__name__, result)
+        return result
+
+    return wrapper
+
+
+class Runner:
+    def __init__(self):
+        self.log_level = os.getenv("LOG_LEVEL", "").upper()
+        self.initial_prompt = os.getenv("INITIAL_PROMPT", "")
+        self.model_context_size = int(os.getenv("MODEL_CONTEXT_SIZE", "0"))
+        self.target_host = os.getenv("TARGET_HOST", "")
+        self.target_host_user = os.getenv("TARGET_HOST_USER", "")
+        self.target_host_user_password = os.getenv("TARGET_HOST_USER_PASSWORD", "")
+        self.target_host_user_keyfile = os.getenv("TARGET_HOST_USER_KEYFILE", "")
+        self.target_host_user_sudo_password = os.getenv(
+            "TARGET_HOST_USER_SUDO_PASSWORD", ""
+        )
+        self.bastion_host = os.getenv("BASTION_HOST", "")
+        self.bastion_host_user = os.getenv("BASTION_HOST_USER", "")
+        self.bastion_host_user_password = os.getenv("BASTION_HOST_USER_PASSWORD", "")
+        self.bastion_host_user_keyfile = os.getenv("BASTION_HOST_USER_KEYFILE", "")
+        self.llm_api_base_url = os.getenv("LLM_API_BASE_URL", "")
+        self.llm_api_key = os.getenv("LLM_API_KEY", "")
+        self.shell_environment = "DEBIAN_FRONTEND=noninteractive ASSUME_YES=1 LC_ALL=C"
+        self.system_prompt = (
+            "You're an autonomous system administrator managing a server non-interactively."
+            "Your objective is print the next command to run to complete the task and NOTHING ELSE!"
+            "You must follow these rules:"
+            "1. If your objective has been completed successfully, print DONE."
+            "2. Prepend privileged actions with sudo."
+            "3. Don't use tools that require interaction with the terminal, like vim or nano."
+            "4. Don't include explanations of anything, only print commands."
+            "5. Don't print multiple commands at one time."
+            "6. If you get a 'Permission denied' error, try a different method."
+            "7. Add -y to package installation commands."
+        )
+        self.system_prompt_summarize = (
+            "You are a helpful AI assistant that summarizes text."
+            "Your objective is to summarize all text that is provided to you as input and NOTHING ELSE!"
+            "You must follow these rules:"
+            "1. Be brief"
+            "2. All summaries must be a single line."
+            "3. Only include the most important and relevant information."
+            "3. Don't be verbose."
+            "4. Don't summarize over multiple lines."
+        )
+
+        required_vars = {
+            "LOG_LEVEL": self.log_level,
+            "INITIAL_PROMPT": self.initial_prompt,
+            "TARGET_HOST": self.target_host,
+            "TARGET_HOST_USER": self.target_host_user,
+            "LLM_API_BASE_URL": self.llm_api_base_url,
+        }
+
+        for name, value in required_vars.items():
+            if not value:
+                raise EnvironmentError(f"{name} not defined!")
+
+    def configure_logging(self) -> None:
+        """Set up logging."""
+        log.basicConfig(
+            format="%(asctime)s %(funcName)s: %(message)s", level=self.log_level.upper()
+        )
+        log.debug(
+            "Logging has been configured at a level of: %s", self.log_level.upper()
+        )
+
+    @log_function_call
+    def initialize(self) -> None:
+        """Run general setup and safety checks."""
+        self.configure_logging()
+        self.wait_for_llm_to_become_available()
+
+    @log_function_call
+    def query_llm(self, prompt) -> str:
+        """
+        Send a prompt to an LLM API and return its reply. LLM API must be OpenAI-compatible.
+
+        Args:
+            base_url (string): The base URL of the LLM API, like "https://codeium.example.com/v1".
+            prompt (list of dicts): The LLM prompt, including system prompt. Previous responses from the
+                                    LLM can also be added as context for new replies in order to mimic
+                                    a conversation. See example below.
+
+        Example:
+            prompt = [
+                {"role": "system", "content": "You're a helpful AI assistant.",},
+                {"role": "user", "content": "What is the capital of Japan?",},
+                {"role": "assistant", "content": "Tokyo",},
+                {"role": "user", "content": "What is its population?",},
+            ]
+
+        Returns:
+            str: LLM's reponse.
+        """
+        client = openai.OpenAI(
+            base_url=self.llm_api_base_url,
+            api_key=self.llm_api_key,
+        )
+
+        llm_reply = client.chat.completions.create(
+            model="llama3.1",
+            messages=prompt,
+        )
+
+        response_string = llm_reply.choices[0].message.content
+        response_string_stripped = strip_eot_from_string(response_string)
+        return response_string_stripped
+
+    @log_function_call
+    def can_llm_be_reached(self) -> bool:
+        """
+        Check if the LLM API can be reached with a quick prompt.
+
+        Returns:
+            bool: True if the API can be reached, False otherwise.
+        """
+        prompt = [
+            {
+                "role": "system",
+                "content": "You're a helpful AI assistant.",
+            },
+            {
+                "role": "user",
+                "content": "Print GOOD and nothing else.",
+            },
+        ]
+        try:
+            self.query_llm(prompt=prompt)
+            return True
+        except openai.InternalServerError:
+            return False
+
+    @log_function_call
+    def wait_for_llm_to_become_available(self) -> None:
+        """
+        Wait in a loop until the LLM API can be reached successfully.
+
+        Raises:
+            Raises a RuntimeError if waiting times out.
+        """
+        log.warning("Checking LLM connectivity...")
+        for i in range(100):
+            if self.can_llm_be_reached() is False:
+                log.warning("Waiting for LLM server to become available ... %s/100", i)
+                time.sleep(10)
+            else:
+                return
+        raise RuntimeError("Timed out waiting for LLM server to become available!")
+
+    @log_function_call
+    def summarize_string(self, string: str) -> str:
+        """
+        Summarize the given string with the LLM API.
+
+        This is done so longer strings can better fit into the LLM's context window during long
+        conversations.
+
+        Args:
+            string (string): The given string to summarize with the LLM.
+
+        Returns:
+            str: The summarized string.
+        """
+        prompt = [
+            {
+                "role": "system",
+                "content": self.system_prompt_summarize,
+            },
+            {
+                "role": "user",
+                "content": string,
+            },
+        ]
+        llm_summarization = self.query_llm(prompt=prompt)
+        log.warning("SSH reply was summarized to: %s", llm_summarization)
+        return llm_summarization
+
+    @log_function_call
+    def context_size_warning_check(self, messages, threshold=0.85) -> bool:
+        """
+        Print a warning if we're about to exceed the context size of the model.
+
+        Args:
+            messages (list of dicts): Counts tokens in the "content" key of each dictionary in the
+                                      provided list to determine if we're near the context size threshold
+                                      of the LLM. See examples below.
+
+            threshold (float): A decimal between 0 and 1 to determine the threshold of the LLM's context
+                               size to warn after. For example, a threshold of "0.85" will warn after
+                               we've exceeded 85% of the LLM's context size. Default is 0.85.
+
+        Examples:
+            messages = [
+                {"role": "system", "content": "You're a helpful AI assistant.",},
+                {"role": "user", "content": "What is the capital of Japan?",},
+                {"role": "assistant", "content": "Tokyo",},
+            ]
+
+        Returns:
+            bool: True if context threshold has been exceeded, False otherwise.
+        """
+        # Skip if the context size hasn't been set.
+        print("model context size is", self.model_context_size)
+        if self.model_context_size == 0:
+            print("Returning false sinze size is 0")
+            return False
+        num_tokens = count_tokens(messages)
+        print("Current token count is ", num_tokens)
+        log.warning("Currently using %s tokens", num_tokens)
+        if num_tokens > (threshold * self.model_context_size):
+            log.warning("REACHED %s OF MODEL CONTEXT SIZE", str(threshold))
+            return True
+        return False
+
+    @log_function_call
+    def run_ssh_cmd(self, connection: fabric.Connection, command: str) -> str:
+        """
+        Run a command over an existing SSH connection and return its output.
+
+        Args:
+            ssh (fabric.Connection): The open SSH connection to use for command execution.
+            command (str): The command to run on the host.
+            sudo_password (str): The password to use to elevate with sudo.
+
+        Returns:
+            str: SSH command output. Both stdout and stderr are combined into a single string.
+        """
+        command = f"{self.shell_environment} ; {command}"
+
+        if self.target_host_user_sudo_password:
+            result = connection.sudo(
+                command,
+                warn=True,
+                pty=True,
+                password=self.target_host_user_sudo_password,
+            )
+        else:
+            result = connection.run(command, warn=True, pty=True)
+
+        # Combine output streams for the LLM.
+        output = result.stdout.strip() + result.stderr.strip()
+        log.debug("run_ssh_cmd output is: %s", output)
+        return output
+
+    @log_function_call
+    def initialize_messages(self) -> list:
+        """
+        Initialize the messages list with the system and user prompts.
+
+        Args:
+            system_prompt (str): The system's prompt.
+            initial_prompt (str): The initial user's prompt.
+
+        Returns:
+            list: Initialized messages list.
+        """
+        return [
+            {"role": "system", "content": self.system_prompt},
+            {"role": "user", "content": self.initial_prompt},
+        ]
+
+    @log_function_call
+    def setup_ssh_connection_params(self) -> dict:
+        """
+        Prepare SSH connection parameters based on the configuration.
+
+        Args:
+            config (Config): Configuration object containing SSH credentials.
+
+        Returns:
+            dict: SSH connection parameters.
+        """
+        if self.target_host_user_keyfile:
+            return {"key_filename": self.target_host_user_keyfile}
+        return {"password": self.target_host_user_password}
+
+    @log_function_call
+    def process_interaction_loop(self, messages: list, connect_args: dict) -> None:
+        """
+        Process the interaction loop with the LLM and SSH server.
+
+        Args:
+            messages (list): The list of messages in the conversation.
+            connect_args (dict): SSH connection parameters.
+        """
+        with fabric.Connection(
+            host=self.target_host,
+            user=self.target_host_user,
+            connect_kwargs=connect_args,
+        ) as ssh:
+            while True:
+                llm_reply = self.query_llm(messages)
+                log.warning("LLM reply was: %s", llm_reply)
+
+                if is_llm_done(llm_reply):
+                    log.critical("All done!")
+                    return
+
+                ssh_reply = self.handle_ssh_command(ssh, llm_reply)
+                update_conversation(messages, llm_reply, ssh_reply)
+                self.context_size_warning_check(messages)
+
+    @log_function_call
+    def handle_ssh_command(self, ssh: fabric.Connection, llm_reply: str) -> str:
+        """
+        Send the LLM reply to the server via SSH and get the server's response.
+
+        Args:
+            ssh (fabric.Connection): The active SSH connection.
+            llm_reply (str): The reply from the LLM.
+
+        Returns:
+            str: The server's response.
+        """
+        ssh_reply = self.run_ssh_cmd(connection=ssh, command=llm_reply)
+        log.warning("SSH reply was: %s", ssh_reply)
+
+        if is_string_too_long(ssh_reply):
+            ssh_reply = self.summarize_string(ssh_reply)
+
+        return ssh_reply
+
+    @log_function_call
+    def main(self):
+        """Main function to initialize and run the system."""
+        self.initialize()
+
+        # Initialize the conversation messages.
+        messages = self.initialize_messages()
+
+        # Setup SSH connection parameters.
+        connect_args = self.setup_ssh_connection_params()
+
+        # Process the interaction loop.
+        self.process_interaction_loop(messages, connect_args)
+
+
+@log_function_call
+def strip_eot_from_string(string: str) -> str:
+    """
+    Strip the <|eot_id|> from the end of the LLM response for more human-readable output.
+
+    Args:
+        string (str): The string you wish to strip the EOT from.
+
+    Returns:
+        str: The input string with <|eot_id|> stripped, if present.
+    """
+    eot_token = "<|eot_id|>"
+    if string.endswith(eot_token):
+        return string[: -len(eot_token)]
+    return string
+
+
+@log_function_call
+def is_string_too_long(string: str, threshold: int = 1000) -> bool:
+    """
+    Determine if the given string is longer than a certain threshold.
+
+    This helps us determine if LLM output needs to be summarized to reduce context size.
+
+    Args:
+        string (string): The input string to check.
+        threshold (int): The input string will be checked if it's longer than the threshold length.
+                         Default is 1000.
+
+    Returns:
+        bool: True if string is longer than the threshold, otherwise returns False.
+    """
+    if len(string) > threshold:
+        return True
+    return False
+
+
+@log_function_call
+def count_tokens(messages) -> int:
+    """
+    Count the number of LLM tokens in the provided dictionary of context.
+
+    Uses the list of dictionaries inside the messages list and counts the tokens in the "content" key.
+
+    Args:
+        messages (list of dicts): Counts tokens in the "content" key of each dictionary in the
+                                  provided list. See examples below.
+
+    Examples:
+        messages = [
+            {"role": "system", "content": "You're a helpful AI assistant.",},
+            {"role": "user", "content": "What is the capital of Japan?",},
+            {"role": "assistant", "content": "Tokyo",},
+        ]
+
+    Returns:
+        int: The number of tokens.
+    """
+    content_string = " ".join(message["content"] for message in messages)
+
+    encoding = tiktoken.encoding_for_model("gpt-4o")
+    tokens = encoding.encode(content_string)
+
+    num_tokens = len(tokens)
+    return num_tokens
+
+
+@log_function_call
+def is_llm_done(llm_reply: str) -> bool:
+    """
+    Check if the LLM has finished with its objective.
+
+    Args:
+        llm_reply (str): The reply from the LLM to analyze to determine if it's finished with its
+                         objective.
+
+    Returns:
+        bool: True if the LLM is finished, False otherwise.
+    """
+    if llm_reply == "DONE":
+        return True
+    return False
+
+
+@log_function_call
+def update_conversation(messages: list, llm_reply: str, ssh_reply: str) -> None:
+    """
+    Update the conversation messages with the LLM's reply and the server's response.
+
+    Args:
+        messages (list): The list of messages in the conversation.
+        llm_reply (str): The reply from the LLM.
+        ssh_reply (str): The server's response.
+    """
+    messages.append({"role": "assistant", "content": llm_reply})
+    messages.append({"role": "user", "content": ssh_reply})
+
+
+if __name__ == "__main__":
+    Runner().main()
---
+++ ssherlock_runner/tests/test_ssherlock_runner.py
@@ -0,0 +1,95 @@
+import importlib
+import logging as log
+import os
+import sys
+from unittest.mock import patch  # Use this for mocking
+
+import pytest
+
+sys.path.insert(1, "../")
+from ssheldon import (
+    Runner,
+    strip_eot_from_string,
+    is_string_too_long,
+    count_tokens,
+    is_llm_done,
+)
+
+
+# Set required environment variables
+os.environ["LOG_LEVEL"] = "WARNING"
+os.environ["INITIAL_PROMPT"] = "test"
+os.environ["TARGET_HOST"] = "test.com"
+os.environ["TARGET_HOST_USER"] = "test"
+os.environ["LLM_API_BASE_URL"] = "test.example.com"
+
+
+def test_strip_eot_from_string():
+    assert strip_eot_from_string("Hello, World!<|eot_id|>") == "Hello, World!"
+    assert strip_eot_from_string("Hello, World!") == "Hello, World!"
+
+
+def test_is_string_too_long():
+    assert is_string_too_long("a" * 1001) is True
+    assert is_string_too_long("a" * 1000) is False
+    assert is_string_too_long("a" * 999) is False
+    assert is_string_too_long("a" * 501, threshold=500) is True
+    assert is_string_too_long("a" * 499, threshold=500) is False
+
+
+def test_count_tokens():
+    messages = [
+        {"role": "system", "content": "You are a helpful AI assistant."},
+        {"role": "user", "content": "What is the capital of Japan?"},
+        {"role": "assistant", "content": "Tokyo"},
+    ]
+    assert count_tokens(messages) == 15
+
+
+def test_context_size_warning_check():
+    os.environ["MODEL_CONTEXT_SIZE"] = "16"
+    runner = Runner()
+    print(runner.model_context_size)
+
+    messages = [
+        {"role": "system", "content": "You're a helpful AI assistant."},
+        {"role": "user", "content": "What is the capital of Japan?"},
+        {"role": "assistant", "content": "Tokyo"},
+    ]
+    assert runner.context_size_warning_check(messages, threshold=0.85) is True
+
+    #  with patch("your_module_name.count_tokens", return_value=80):
+    #      caplog.clear()
+    #      context_size_warning_check(messages, threshold=0.85)
+    #      assert "REACHED 0.85 OF MODEL CONTEXT SIZE" not in caplog.text
+
+
+def test_is_llm_done():
+    assert is_llm_done("DONE") is True
+    assert is_llm_done("Not done") is False
+    assert is_llm_done("done") is False
+
+
+def test_initialize_messages():
+    system_prompt = "System prompt here."
+    initial_prompt = "Initial prompt here."
+
+    messages = initialize_messages(system_prompt, initial_prompt)
+    assert len(messages) == 2
+    assert messages[0]["role"] == "system"
+    assert messages[0]["content"] == system_prompt
+    assert messages[1]["role"] == "user"
+    assert messages[1]["content"] == initial_prompt
+
+
+def test_setup_ssh_connection_params_with_keyfile(config):
+    config.target_host_user_keyfile = "/path/to/keyfile"
+    connect_args = setup_ssh_connection_params()
+    assert connect_args["key_filename"] == "/path/to/keyfile"
+
+
+def test_setup_ssh_connection_params_with_password(config):
+    config.target_host_user_keyfile = ""
+    config.target_host_user_password = "supersecurepassword"
+    connect_args = setup_ssh_connection_params()
+    assert connect_args["password"] == "supersecurepassword"
