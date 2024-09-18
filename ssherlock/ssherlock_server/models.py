2d72ad5d7ca7d65acfcbb6259f32b598e0eac124
Austin Kelley <hxyz@protonmail.com>
2024-09-17 17:21:02 -0700

    models: sort possible job statuses

--- ssherlock/ssherlock_server/models.py
+++ ssherlock/ssherlock_server/models.py
@@ -119,12 +119,12 @@ class Job(models.Model):
         "Amount of time job took to complete", blank=True, null=True, editable=False
     )
     STATUS_CHOICES = [
+        ("CA", "CANCELED"),
+        ("CE", "CONTEXT_EXCEEDED"),
+        ("CO", "COMPLETED"),
+        ("FA", "FAILED"),
         ("PE", "PENDING"),
         ("RU", "RUNNING"),
-        ("CA", "CANCELED"),
-        ("CO", "COMPLETED"),
-        ("CE", "CONTEXT_EXCEEDED"),
-        ("FA", "FAILED"),
     ]
     status = models.CharField(
         "Current status of job",
