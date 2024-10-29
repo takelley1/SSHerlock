9d04863fe9f8e87d5b5943cd3fcfc41cec4953c5
Austin Kelley <troy.kelley@caci.com>
2024-10-28 22:23:10 -0400

    views: fix for linting

--- ssherlock/ssherlock_server/views.py
+++ ssherlock/ssherlock_server/views.py
@@ -4,7 +4,7 @@
 
 import json
 import os
-from django.http import Http404, JsonResponse, HttpResponse, HttpResponseBadRequest
+from django.http import Http404, JsonResponse, HttpResponse
 from django.views.decorators.http import require_http_methods
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import get_object_or_404, redirect, render
